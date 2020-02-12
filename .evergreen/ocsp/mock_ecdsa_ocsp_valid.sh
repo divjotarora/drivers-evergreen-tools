41edb6afd48121fe71c4cfb0e771c8412a7fe863
Vincent Kam <vincent.kam@10gen.com>
2020-02-12 14:57:46 -0500

    Update scripts for ECDSA/Delegates

--- .evergreen/ocsp/mock_ecdsa_ocsp_valid.sh
+++ .evergreen/ocsp/mock_ecdsa_ocsp_valid.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 # Use the CA as the OCSP responder (see https://community.letsencrypt.org/t/unable-to-verify-ocsp-response/7264)
 python3 ocsp_mock.py \
-  --ca_file ca_ocsp.pem \
-  --ocsp_responder_cert ca_ocsp.crt \
-  --ocsp_responder_key ca_ocsp.key \
+  --ca_file ecdsa-ca-ocsp.pem \
+  --ocsp_responder_cert ecdsa-ca-ocsp.crt \
+  --ocsp_responder_key ecdsa-ca-ocsp.key \
    -p 8100 \
-   -v \
-   --fault revoked
+   -v
