58fff342f4330c13ecbdaaf5fa248417c5717ac7
Vincent Kam <vincent.kam@10gen.com>
2020-02-12 15:56:51 -0500

    Update comments

--- .evergreen/ocsp/mock_ecdsa_ocsp_valid.sh
+++ .evergreen/ocsp/mock_ecdsa_ocsp_valid.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-# Use the CA as the OCSP responder (see https://community.letsencrypt.org/t/unable-to-verify-ocsp-response/7264)
 python3 ocsp_mock.py \
   --ca_file ecdsa-ca-ocsp.pem \
   --ocsp_responder_cert ecdsa-ca-ocsp.crt \
--- .evergreen/ocsp/mock_ocsp_revoked.sh
+++ .evergreen/ocsp/mock_ocsp_revoked.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-# Use the CA as the OCSP responder (see https://community.letsencrypt.org/t/unable-to-verify-ocsp-response/7264)
 python3 ocsp_mock.py \
   --ca_file ca_ocsp.pem \
   --ocsp_responder_cert ca_ocsp.crt \
