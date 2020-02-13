90e1fdc4bd6e032a14d06a8bbc115817492ec975
Vincent Kam <vincent.kam@10gen.com>
2020-02-13 16:50:58 -0500

    Move and update scripts to account for new file names and directory structure

--- .evergreen/ocsp/ecdsa/mock_delegate_valid.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-python3 ocsp_mock.py \
-  --ca_file ecdsa-ca-ocsp.pem \
-  --ocsp_responder_cert ecdsa-ocsp-responder.crt \
-  --ocsp_responder_key ecdsa-ocsp-responder.key \
-   -p 8100 \
-   -v
--- .evergreen/ocsp/rsa/mock_ocsp_revoked.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-python3 ocsp_mock.py \
-  --ca_file ca_ocsp.pem \
-  --ocsp_responder_cert ca_ocsp.crt \
-  --ocsp_responder_key ca_ocsp.key \
-   -p 8100 \
-   -v \
-   --fault revoked
